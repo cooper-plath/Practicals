48663cfb93111adba64bb6e001066de6d2908864
Plath <cooper.plath@my.jcu.edu.au>
2020-08-31 20:44:08 +1000

    Practical 4

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,38 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+"""
+a. 3
+b. 2
+c. 1
+d. 2
+e. 
+f. true
+g. false
+h. false
+i. adds to list
+
+"""
+
+
+# 1.
+
+# numbers[0] = "10"
+#
+# print(numbers)
+
+# 2.
+#
+# numbers[-1] = 1
+#
+# print(numbers)
+
+# 3.
+#
+# numb = numbers[2:7]
+#
+# print(numb)
+
+# 4.
+
+# print(9 in numbers)
---
+++ Prac_04/total_income.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    Total_Month_Amount = int(input("How many months? "))
+
+    for month in range(1, Total_Month_Amount + 1):
+        income = float(input("Enter income for month {} :".format(str(month))))
+        incomes.append(income)
+
+    Print_Income_Report(Total_Month_Amount, incomes)
+
+
+def Print_Income_Report(Total_Month_Amount, incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, Total_Month_Amount + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
